8062032fd6df9b761a17562c5a7340df2217330c
cerebus <cerebus@student.21-school.ru>
2021-11-02 18:24:18 +0300

    Made mutantStack.hpp

--- ex02/MutantStack.cpp
+++ ex02/MutantStack.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "MutantStack.hpp"
+
+//template<typename T, typename Cont>
+//MutantStack<T, Cont>::MutantStack(): std::stack<T>() {}
+
+//template<typename T, typename Cont>
+//MutantStack<T, Cont>::~MutantStack() {}
+
+//template<typename T, typename Cont>
+//MutantStack<T, Cont>::MutantStack(const MutantStack &orig) : std::stack<T>(orig) {
+//	*this = orig;
+//}
+//
+//template<typename T, typename Cont>
+
+
--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,9 +6,38 @@
 #define MUTANTSTACK_HPP
 
 
-class MutantStack {
+#include <stack>
 
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>  {
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator begin(void)
+	{
+		return std::stack<T>::c.begin();
+	}
+	iterator end(void)
+	{
+		return std::stack<T>::c.end();
+	}
+
+	MutantStack() : std::stack<T>() {}
+	~MutantStack() {}
+	MutantStack(const MutantStack &orig) : std::stack<T, Container>() {
+		this->c = orig.c;
+	}
+	//MutantStack &operator=(const MutantStack &orig);
+
+	MutantStack <T>&operator=(const MutantStack <T>&orig) {
+		if (this == &orig)
+			return *this;
+		this->c = orig.c;
+		return *this;
+	}
 };
+
+
 
 
 #endif //DAY08_MUTANTSTACK_HPP
